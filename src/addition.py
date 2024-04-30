c326d0ce2f825f29b9ddc6a1a23ec0a288054f98
KundanAntyakula <168166698+KundanAntyakula@users.noreply.github.com>
2024-04-30 11:29:38 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,10 +1,11 @@
 # app.py
 # This is a test commit
+#test
 def add(a, b):
     return a + b
     
 
-#unit testing
+#unit_testing
 def test_add():
     assert add(1, 2) == 3  
     assert add(1, -1) == 0
